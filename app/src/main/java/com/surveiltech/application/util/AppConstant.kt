50de755b62c3ba2665c2aba3a6cae43cdd92b844
clemane <clement.lemane@gmail.com>
2024-06-17 13:32:30 -0400

    testAPI OpenAI

--- app/src/main/java/com/surveiltech/application/util/AppConstant.kt
+++ app/src/main/java/com/surveiltech/application/util/AppConstant.kt
@@ -2,4 +2,4 @@ package com.surveiltech.application.util
 
 const val BASE_URL = "https://api.openai.com/v1/"
 const val CHATGPT_MODEL = "gpt-3.5-turbo"
-const val OPENAI_API_KEY = "sk-proj-REcTtaYgY12yWMU6ZJxST3BlbkFJCBc1ArifmIcae2JdWBwd"
+const val OPENAI_API_KEY = "sk-proj-MOWgKBauKdc7YflYZ0dET3BlbkFJ0zGCdj6zrMTNfGfXYFkk"
