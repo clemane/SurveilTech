4114780668fb88db2dd1b9d7bbfaa5970bc0476e
clemane <clement.lemane@gmail.com>
2024-06-17 13:04:29 -0400

    apikey

--- app/src/main/java/com/surveiltech/application/util/AppConstant.kt
+++ app/src/main/java/com/surveiltech/application/util/AppConstant.kt
@@ -2,4 +2,4 @@ package com.surveiltech.application.util
 
 const val BASE_URL = "https://api.openai.com/v1/"
 const val CHATGPT_MODEL = "gpt-3.5-turbo"
-const val OPENAI_API_KEY = ""
+const val OPENAI_API_KEY = "sk-proj-SrvyXW0J8wdGGISS1Ws5T3BlbkFJKOaLPTPHBuhdQuhhIZQy"
